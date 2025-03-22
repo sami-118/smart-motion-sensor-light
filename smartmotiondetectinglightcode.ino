2cacf27c00d8aff539520df75a279a1170557247
HeISeNBeRg <sm3230147@gmail.com>
2025-03-22 17:55:29 +0600

    Add files via upload

---
+++ smartmotiondetectinglightcode.ino
@@ -0,0 +1,21 @@
+int relay = 12;
+int pir = 11;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(relay, OUTPUT);
+  pinMode(pir, INPUT);
+}
+
+void loop() {
+  if (digitalRead(pir) == LOW) { // Reversed logic
+    Serial.println("Motion detected - Turning ON light");
+    digitalWrite(relay, LOW);  // If relay is active-low
+  } 
+  else {
+    Serial.println("No motion - Turning OFF light");
+    digitalWrite(relay, HIGH); // If relay is active-low
+  }
+
+  delay(500); // Small delay for stability
+}
